b8d10c0d73c8599bc740d5728bb2627490230d3c
gaozhi <15575565410@163.com>
2020-01-15 20:33:28 +0800

    将本地工程添加到git

---
+++ git-test/src/com/gao/git/GitClass.java
@@ -0,0 +1,15 @@
+/**
+ * Author:   Administrator
+ * Date:     2020/1/15 20:26
+ * Description:
+ * History:
+ */
+package com.gao.git;
+
+public class GitClass {
+    public static void main(String[] args) {
+        System.out.println("你好我的世界:");
+        System.out.println("你好我的世界:");
+        System.out.println("你好我的世界:");
+    }
+}
